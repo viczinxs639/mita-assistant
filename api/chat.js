aed0b052c7e29f1bec7df24743f6695b8426604f
viczinxs639 <victorgabrielcandido0@gmail.com>
2026-05-04 20:46:00 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     }));
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContentt?key=${apiKey}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
